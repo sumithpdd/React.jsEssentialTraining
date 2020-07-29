0d03fcbdb877efad5ef0aab73b26d906b449772d
Sumith Damodaran <sumithpd@gmail.com>
2020-07-29 22:02:51 +0200

    using setState

--- src/Library.jsx
+++ src/Library.jsx
@@ -1,20 +1,39 @@
 import React from "react";
 import Book from "./Book";
 
-function Library({ books }) {
-  return (
-    <div>
-      <h1>Welcome to the Library</h1>
-      {books.map((book, i) => (
-        <Book
-          key={i}
-          title={book.title}
-          author={book.author}
-          pages={book.pages}
-        />
-      ))}
-    </div>
-  );
+class Library extends React.Component {
+  state = {
+    open: true
+  };
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {
+  //     open: true
+  //   };
+  //   this.toggleOpenClosed = this.toggleOpenClosed.bind(this);
+  // }
+  toggleOpenClosed = () => {
+    this.setState(prevState => ({
+      open: !prevState.open
+    }));
+  };
+  render() {
+    const { books } = this.props;
+    return (
+      <div>
+        <h1>The Library is {this.state.open ? "open" : "closed"}</h1>
+        <button onClick={this.toggleOpenClosed}>Change</button>
+        {books.map((book, i) => (
+          <Book
+            key={i}
+            title={book.title}
+            author={book.author}
+            pages={book.pages}
+          />
+        ))}
+      </div>
+    );
+  }
 }
 
 export default Library;
